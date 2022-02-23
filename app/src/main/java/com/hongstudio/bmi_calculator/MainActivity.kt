e396e3478c15a91e8bf425fbd1b82ac5eaf3257d
sdh041 <sdh041@ladaka.com>
2022-02-23 10:59:17 +0900

    Initial commit

---
+++ app/src/main/java/com/hongstudio/bmi_calculator/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.hongstudio.bmi_calculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
---
+++ app/src/main/java/com/hongstudio/bmi_calculator/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.hongstudio.bmi_calculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_start.*
+
+class MainFragment : Fragment() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        button_next.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_resultFragment)
+        }
+    }
+
+}
